6edb95e63094ba9932726662c7eb0c81e50e87f8
Christopher Martin <cgmartin@gmail.com>
2015-12-31 17:43:43 -0500

    active menu item styling

---
+++ scripts/is-current-menu.js
@@ -0,0 +1,21 @@
+/**
+ * Helper: is_current_menu(menu)
+ *
+ * To be used as an alternative to the `is_current()` helper,
+ * since that one doesn't seem to work with our layout's "menu" config.
+ */
+function isCurrentMenu(menu){
+  var path = this.url_for(menu) || '';
+
+  // filter the input path and the current path
+  var paths = [path, this.path].map(function(p) {
+    return p
+      .replace(/index.html$/, '')
+      .replace(/\/$/, '')
+      .replace(/^\//, '');
+  });
+
+  return paths[0] === paths[1];
+}
+
+hexo.extend.helper.register('is_current_menu', isCurrentMenu);
